de66fae750039aa7834f45595c3ddef7639c214d
oZeroLuck <50614854+oZeroLuck@users.noreply.github.com>
2021-04-12 11:28:34 +0200

    Testing table

---
+++ src/app/mock-data.ts
@@ -0,0 +1,7 @@
+import {UsersClass} from './usersClass';
+
+export const UserList: UsersClass[] = [
+  {name: 'Giada', surname: 'Sborchia'},
+  {name: 'Francesca', surname: 'Mecca'},
+  {name: 'Alice', surname: 'Erba'}
+];
---
+++ src/app/usersClass.ts
@@ -0,0 +1,4 @@
+export class UsersClass {
+  name: string;
+  surname: string;
+}
