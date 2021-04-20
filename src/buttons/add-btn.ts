57aaeadc6a3cdf102fd208db3d877cd52ca878f1
oZeroLuck <50614854+oZeroLuck@users.noreply.github.com>
2021-04-20 15:28:37 +0200

    Added new button configs

---
+++ src/buttons/add-btn.ts
@@ -0,0 +1,8 @@
+import { MyButtonConfig } from './button-config';
+
+export const AddBtn: MyButtonConfig = {
+  customCssClass: 'btn btn-success',
+  text: 'Add',
+  icon: 'add',
+  action: 'add'
+};
---
+++ src/buttons/cancel-btn.ts
@@ -0,0 +1,8 @@
+import { MyButtonConfig } from './button-config';
+
+export const CancelBtn: MyButtonConfig = {
+  customCssClass: 'btn btn-warning',
+  text: 'cancel',
+  icon: 'close',
+  action: 'cancel'
+};
