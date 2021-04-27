f30f97263b90be557edf83dbe2d6ea88ea9fbedd
oZeroLuck <50614854+oZeroLuck@users.noreply.github.com>
2021-04-27 16:28:00 +0200

    Added testing buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import {Component} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {AuthenticationService} from './resources/services/authentication.service';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +10,18 @@ import {Component} from '@angular/core';
 export class AppComponent{
   title = 'AngularFirstSteps';
 
-  constructor() {}
+  constructor(public http: HttpClient,
+              public authService: AuthenticationService) {}
 
+  public ping(): void {
+    this.http.get('api/usersList')
+      .subscribe(
+        data => console.log(data),
+        err => console.log(err)
+      );
+  }
+
+  public isLoggedIn(): void {
+    console.log(this.authService.isUserLoggedIn());
+  }
 }
