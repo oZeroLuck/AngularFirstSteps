55e705f81f28ce102f0de8cf568efa1edbd6668e
oZeroLuck <50614854+oZeroLuck@users.noreply.github.com>
2021-04-28 16:16:41 +0200

    Changed button style

--- src/app/resources/custom-configs/buttons/logout-btn.ts
+++ src/app/resources/custom-configs/buttons/logout-btn.ts
@@ -1,7 +1,7 @@
 import { MyButtonConfig } from './button-config';
 
 export const LogoutBtn: MyButtonConfig = {
-  customCssClass: 'btn btn-danger',
+  customCssClass: 'btn btn-outline-danger',
   icon: 'logout',
   text: 'Logout',
   action: 'logout'
