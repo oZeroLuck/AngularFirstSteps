3942daa98d10eb76946c81e53a31d04181de3475
oZeroLuck <50614854+oZeroLuck@users.noreply.github.com>
2021-04-28 14:53:39 +0200

    Added

---
+++ src/app/resources/custom-configs/buttons/save-btn.ts
@@ -0,0 +1,8 @@
+import { MyButtonConfig } from './button-config';
+
+export const SaveBtn: MyButtonConfig = {
+  customCssClass: 'btn btn-primary',
+  icon: 'save',
+  text: 'Save',
+  action: 'save'
+};
