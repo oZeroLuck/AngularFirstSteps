25d83128f128cedb9608d2749625c657493702e4
oZeroLuck <50614854+oZeroLuck@users.noreply.github.com>
2021-04-30 11:31:00 +0200

    Routing test

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,7 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 
 import { AdminHomepageComponent } from './pages/admin-homepage/admin-homepage.component';
 import { CarParkComponent } from './pages/car-park/car-park.component';
-import { FormComponent } from './pages/vehicle-edit/form.component';
+import { FormComponent } from './pages/form-page/form.component';
 import { ReservationsComponent } from './pages/reservations/reservations.component';
 import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { ResFormComponent } from './pages/res-form/res-form.component';
@@ -12,16 +12,15 @@ import { ProfileComponent } from './pages/profile/profile.component';
 
 import { AdminAuthGuard } from './resources/services/authentication/admin-auth-guard.service';
 import { BasicAuthGuard } from './resources/services/authentication/basic-auth-guard.service';
-import {ModalBasicComponent} from './components/modal-password/modal-basic.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/login', pathMatch: 'full'},
   {path: 'login', component: LoginComponent},
   {path: 'dashboard', component: NavBarComponent, canActivate: [BasicAuthGuard]},
-  {path: 'homepage/customers',
+  {path: 'homepage/customers', canActivate: [AdminAuthGuard],
     children: [
       {path: '', component: AdminHomepageComponent, canActivate: [AdminAuthGuard]},
-      {path: 'profile/:id', component: ProfileComponent},
+      {path: 'profile/:id', canActivate: [BasicAuthGuard], component: ProfileComponent},
       {path: 'reservations', canActivate: [BasicAuthGuard], children: [
           {path: ':userId', children: [
               {path: '', component: ReservationsComponent},
@@ -35,8 +34,8 @@ const routes: Routes = [
   {path: 'carPark', canActivate: [BasicAuthGuard],
     children: [
       {path: '', component: CarParkComponent},
-      {path: ':class/:action', component: FormComponent},
-      {path: ':class/:action/:id', component: FormComponent},
+      {path: ':class/:action', component: FormComponent, canActivate: [AdminAuthGuard]},
+      {path: ':class/:action/:id', component: FormComponent, canActivate: [AdminAuthGuard]},
     ]},
   {path: 'profile', component: ProfileComponent, canActivate: [BasicAuthGuard]}
 ];
