cc356c35b48f6f4874f52874862e94f2d14bd0dd
oZeroLuck <50614854+oZeroLuck@users.noreply.github.com>
2021-04-30 11:26:26 +0200

    Added

---
+++ src/app/resources/custom-configs/buttons/back-btn.ts
@@ -0,0 +1,8 @@
+import { MyButtonConfig } from './button-config';
+
+export const BackBtn: MyButtonConfig = {
+  customCssClass: 'btn btn-outline-dark',
+  icon: 'arrow_back',
+  text: 'Back',
+  action: 'back'
+};
